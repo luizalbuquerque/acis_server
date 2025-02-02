80583bbf5bf44e133666e4daced49bd95a91b951
luizalbuquerque <luiz.info.2016@gmail.com>
2025-02-02 08:01:38 -0300

    Ajustando passo 5

--- setup_data.sh
+++ setup_data.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
-
-
 set -x
 
-
-echo "================================="
+echo "=============================="
 echo "Garantindo permissões de execução"
-echo "================================="
-
+echo "=============================="
 
 # Dá permissão de execução para todos os scripts .sh no diretório atual e subdiretórios
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
-
 # Dá permissão de leitura e execução para todos os arquivos na pasta libs
 chmod +r ./libs/*.jar
 
-echo ""
 echo "Permissões ajustadas com sucesso."
-echo ""
 
 
-
-echo "=================================="
-echo "Pré Setup: Validando se o container está ativo"
-echo "=================================="
+echo =================================
+echo Pré Setup: Validando se o container está ativo
+echo =================================
 docker ps | grep acis_database > /dev/null
 if [ $? -ne 0 ]; then
     echo "Container acis_database não está ativo. Tentando iniciar..."
@@ -39,16 +31,12 @@ else
     echo "Container acis_database já está ativo"
 fi
 
-echo ""
 echo "Aguardando o MySQL inicializar..."
 sleep 10
-echo
 
-
-echo ""
-echo "================================="
-echo "Passo 1: Copiando scripts SQL"
-echo "================================="
+echo =============================
+echo Passo 1: Copiando scripts SQL
+echo =============================
 docker cp ./sql acis_database:/tmp/sql
 if [ $? -ne 0 ]; then
     echo "Passo 1: Falhou"
@@ -57,11 +45,10 @@ else
     echo "Passo 1: Sucesso"
 fi
 
-echo ""
-echo "================================="
-echo "Passo 2: Verificando arquivos copiados"
-echo "================================="
-docker exec acis_database ls -l /tmp/sql
+echo =============================
+echo Passo 2: Verificando arquivos copiados
+echo =============================
+docker exec acis_database ls -l tmp/sql
 if [ $? -ne 0 ]; then
     echo "Passo 2: Falhou"
     exit 1
@@ -69,12 +56,9 @@ else
     echo "Passo 2: Sucesso"
 fi
 
-
-
-echo ""
-echo "================================="
-echo "Passo 3: Criando banco de dados se não existir"
-echo "================================="
+echo =============================
+echo Passo 3: Criando banco de dados se não existir
+echo =============================
 mysql -u root -proot -P 3333 -e "CREATE DATABASE IF NOT EXISTS acis;"
 if [ $? -ne 0 ]; then
     echo "Passo 3: Falhou"
@@ -83,12 +67,9 @@ else
     echo "Passo 3: Sucesso"
 fi
 
-
-
-echo ""
-echo "================================="
-echo "Passo 4: Validando se o banco foi criado"
-echo "================================="
+echo =============================
+echo Passo 4: Validando se o banco foi criado
+echo =============================
 mysql -u root -proot -P 3333 -e "SHOW DATABASES;"
 if [ $? -ne 0 ]; then
     echo "Passo 4: Falhou"
@@ -97,12 +78,9 @@ else
     echo "Passo 4: Sucesso"
 fi
 
-
-
-echo ""
-echo "================================="
-echo "Passo 5: Executando scripts SQL no container"
-echo "================================="
+echo =============================
+echo Passo 5: Executando scripts SQL no container
+echo =============================
 docker exec acis_database bash -c 'for file in /tmp/sql/*.sql; do if [ -f "$file" ]; then echo "Importando $file..."; mysql -u root -proot acis < "$file"; else echo "Nenhum arquivo encontrado em /tmp/sql/*.sql"; fi; done'
 if [ $? -ne 0 ]; then
     echo "Passo 5: Falhou"
@@ -111,12 +89,9 @@ else
     echo "Passo 5: Sucesso"
 fi
 
-
-
-echo ""
-echo "================================="
-echo "Passo 6: Validando tabelas criadas no banco"
-echo "================================="
+echo =============================
+echo Passo 6: Validando tabelas criadas no banco
+echo =============================
 mysql -u root -proot -P 3333 -e "USE acis; SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'acis';"
 if [ $? -ne 0 ]; then
     echo "Passo 6: Falhou"
@@ -125,12 +100,15 @@ else
     echo "Passo 6: Sucesso"
 fi
 
+echo ==================================
+echo SETUP data finalizado com sucesso!
+echo ==================================
 
+echo xxxxxxxxxxxxx
 
-echo ""
-echo "================================="
-echo "Permissionando LoginLoop.sh..."
-echo "================================="
+echo ===============================
+echo Permissionando LoginLoop.sh...
+echo ===============================
 
 # Garantir permissão e formato correto para o script login/LoginLoop.sh
 if [ -f "./login/LoginLoop.sh" ]; then
@@ -150,3 +128,4 @@ else
     echo "Erro: O arquivo login/LoginLoop.sh não foi encontrado!"
     exit 1
 fi
+

4295885e44cdc4c53009e2ae1c14de47777a2610
luizalbuquerque <luiz.info.2016@gmail.com>
2025-02-02 15:46:25 -0300

    nohup servers teste

--- login/LoginServer_loop.sh
+++ login/LoginServer_loop.sh
@@ -4,31 +4,29 @@ echo "=============================="
 echo "Iniciando aCis LoginServer Console"
 echo "=============================="
 
-while true; do
-    # Verifica a versão do Java
-    java -version
+# Verifica a versão do Java
+java -version
 
-    # Inicia o LoginServer
-   nohup  java -Xmx32m -cp "./libs/*" net.sf.l2j.loginserver.LoginServer
-    exit_code=$?
+# Inicia o LoginServer em segundo plano e libera o terminal
+nohup java -Xmx32m -cp "./libs/*" net.sf.l2j.loginserver.LoginServer > loginserver.out 2>&1 &
 
-    if [ $exit_code -eq 2 ]; then
-        echo
-        echo "Admin solicitou reinício. Reiniciando..."
-        echo
-        continue
-    elif [ $exit_code -eq 1 ]; then
-        echo
-        echo "O servidor foi encerrado de forma anormal. Encerrando script."
-        echo
-        break
-    else
-        echo
-        echo "Servidor encerrado normalmente."
-        echo
-        break
-    fi
-done
+# Aguarda o término do processo em segundo plano e obtém o código de saída
+wait $!
+exit_code=$?
+
+if [ $exit_code -eq 2 ]; then
+    echo
+    echo "Admin solicitou reinício. Reiniciando..."
+    echo
+elif [ $exit_code -eq 1 ]; then
+    echo
+    echo "O servidor foi encerrado de forma anormal. Encerrando script."
+    echo
+else
+    echo
+    echo "Servidor encerrado normalmente."
+    echo
+fi
 
 echo "=============================="
 echo "Script encerrado."
